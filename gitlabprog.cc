979d0e9a0b6f2200f1c4a648c6c3449aa0710406
Matthew Carpenter <mc733322@ohio.edu>
2023-09-19 13:23:58 -0400

    comment

---
+++ gitlabprog.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Matthew Carpenter>
+ *        Date: <09/19/23>
+ * Description: This is for Lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
